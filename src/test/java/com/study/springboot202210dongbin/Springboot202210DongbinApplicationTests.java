07c8bab7f84d5893f3b2df7f5b2c29ba2dee4b26
ornornro <roornorn@gmail.com>
2022-12-07 21:25:05 +0900

    Initial commit

---
+++ src/test/java/com/study/springboot202210dongbin/Springboot202210DongbinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.springboot202210dongbin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot202210DongbinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
