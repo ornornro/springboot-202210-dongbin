fb372bb50196dd544b85f946cd18bac1a8ef1d36
ornornro <roornorn@gmail.com>
2022-12-13 21:13:42 +0900

    springBoot end

---
+++ src/main/java/com/study/springboot202210dongbin/web/dto/RequestStudyDto.java
@@ -0,0 +1,10 @@
+package com.study.springboot202210dongbin.web.dto;
+
+import lombok.Data;
+
+@Data
+public class RequestStudyDto {
+    private String name;
+    private String email;
+    private String address;
+}
